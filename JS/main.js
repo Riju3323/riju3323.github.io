b9b18464d25ba852a32559842bf127f76942ff91
Riju <87523667+Riju3323@users.noreply.github.com>
2023-03-19 14:42:52 +0530

    Some Modifications

--- JS/main.js
+++ JS/main.js
@@ -18,3 +18,29 @@ menuLinks.forEach((menuLink) => {
     navbarMenu.classList.remove('active');
   });
 });
+
+//Change Background Header - Make Header Sticky
+
+function scrollHeader() {
+  const header = document.getElementById('header');
+  this.scrollY >= 20? header.classList.add('active') : header.classList.remove('active');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+//Hero Type Effect
+
+const typed = document.querySelector('.typed');
+
+if(typed) {
+  let typed_strings = typed.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed',{
+    strings: typed_strings,
+    loop:true,
+    typeSpeed: 20,
+    backSpeed: 30,
+    backDelay: 400
+  })
+
+}
